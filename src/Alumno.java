bcec4f61f3fde3d5cbc9b3951d5522a0726cbb7d
Mateo Carballo Alonso <a22mateoca@ATEIS.LOCAL>
2023-01-31 20:38:28 +0100

    .

--- src/Alumno.java
+++ src/Alumno.java
@@ -1,2 +1,38 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Alumno {
+    int numerodeMaterias=0;
+    String fechaNacimiento="DD/MM/AAAA";
+    String nombre="*";
+    int edad=0;
+    String[] materias=new String[numerodeMaterias];
+    int [] notas=new int[numerodeMaterias];
+
+
+    public void setEdad(int edad) {
+        if (edad>18) {
+            this.edad = edad;
+        }else{
+            System.out.println("Escribe una valor valido (valor>18)");
+        }
+    }
+
+    public Alumno(){
+
+    }
+
+    public Alumno(int numerodeMaterias, String fechaNacimiento, String nombre, int edad, String[] materias, int[] notas) {
+        this.numerodeMaterias = numerodeMaterias;
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.materias = materias;
+        this.notas = notas;
+    }
+
+
+    public static void escribir(){
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int numeroAlumnos=0;
+
+
+        System.out.println("Cuantos alumnos ?");
+        numeroAlumnos=Integer.parseInt(br.readLine());
+        Alumno[] misAlumnos = new Alumno[numeroAlumnos];
+
+        for (Alumno a:misAlumnos){
+
+        }
+
+            /*
+
+            Product[] obj = new Product[5] ;
+
+            System.out.println("Visualización con forEach:");
+            for (Product p:obj){
+            p.display();
+            }
+             */
+        }
     }
 }
